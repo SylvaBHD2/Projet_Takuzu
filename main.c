f44b81d6ddf0ea653f737b6cee07b9de1094b0ef
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2022-04-06 16:41:20 +0200

    menu

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include "P3.h"
 
 int main() {
-    printf("Hello, TAKUZU!\n");
+    int chc;
+    printf("\n###--------------------------------------------------------------------####\n\nBienvenue dans le jeu du TAKUZU!\n");
+    printf("Veuillez choisir une action :\n");
+    printf("1 - jouer au jeu soi meme \n2 - voir une grille \n3 - resolution auto");
+    printf("\n : ");
+    scanf("%d",&chc);
+
+    switch (chc){
+        case 1:{
+            printf("Cette reponse est en developpement");
+            break;}
+
+        case 2:{
+            printf("Cette reponse est en developpement");
+            break;}
+
+        case 3:{
+            printf("Cette reponse est en developpement");
+//            P3();
+            break;
+        }
+        default : printf("\nCe numero d'exercice n'existe pas. \n");
+    }
     return 0;
 }
