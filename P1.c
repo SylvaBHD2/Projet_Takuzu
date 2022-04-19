6992b762b483141cc34c017b46a07cb21b185240
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2022-04-19 22:47:58 +0200

    P2 Alpha + p1 change

--- P1.c
+++ P1.c
@@ -6,12 +6,21 @@
 #define T_4 4
 #include "P2.h"
 
+//int CoupValide(int i,int j,int essai, int TabMask[T_4][T_4] ,int TabSolution[T_4][T_4],size) {
+//    // retourne 1 si le coup est correct, 0 si valide mais inccorect, et si faux
+//    if (TabSolution[i][j] == essai)
+//        return 1;
+//    else {
+//        // algo de décision
+//        if (nombreSignificatifLigne(TabSolution,TabMask, ))
+//            return -1;
+//
+//    return(0);
+//}
+
 void P1(){
     int Tab[T_4][T_4];
     remplirGrille(Tab,T_4);
-    Tab[1][2]=1;
-    Tab[2][1]=1;
-    Tab[2][3]=1;
     afficherGrille(Tab, T_4);
     masquerGrille(Tab, T_4);
 }
